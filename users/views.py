7b82e2829f3ca4a50478890f9d94644bb8bfee06
borboletinha <painmakesmeflee@gmail.com>
2021-11-22 20:56:53 +0300

    Altered the authentication app view #2

--- users/views.py
+++ users/views.py
@@ -7,14 +7,12 @@ from .serializers import RegistrationSerializer
 @api_view(['POST'])
 def registration_view(request):
     serializer = RegistrationSerializer(data=request.data)
-    data = {}
     if serializer.is_valid():
         user = serializer.save()
-        data['first_name'] = user.first_name
-        data['last_name'] = user.last_name
-        data['username'] = user.username
-        data['email'] = user.email
-        data['success'] = True
+        data = {'success': True, 'response': 'The user is successfully created',
+                'first_name': user.first_name, 'last_name': user.last_name,
+                'username': user.username, 'email': user.email}
     else:
-        data = serializer.errors
+        data = {'success': False, 'response': "The user isn't created'"}
+        data.update(serializer.errors)
     return Response(data, status=status.HTTP_201_CREATED)
