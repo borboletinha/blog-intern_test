b3148de9064c2cec39f4852e295c425da5931538
borboletinha <painmakesmeflee@gmail.com>
2021-11-26 13:21:17 +0300

    Altered accesstoken lifetime

--- testblog/settings.py
+++ testblog/settings.py
@@ -59,11 +59,11 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
-    )
+    ),
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
